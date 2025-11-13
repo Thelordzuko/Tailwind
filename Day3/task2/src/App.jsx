e192651b6acbf2cdbeeb840fde632a735a02de39
Oluwaseyi Alebiosu <alebiosuseyi20@gmail.com>
2025-11-13 12:09:58 +0100

    README added

--- Day3/task2/src/App.jsx
+++ Day3/task2/src/App.jsx
@@ -3,3 +3,62 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+export default function LoginForm() {
+  return (
+    <div className="w-full h-180 bg-red-100 flex justify-center items-center">
+      <form className="bg-white w-[350px] rounded-2xl shadow-xl p-8 flex flex-col gap-6">
+     
+        <h2 className="text-2xl font-bold text-gray-800 text-center">
+          Login
+        </h2>
+
+       
+        <div className="flex flex-col">
+          <label className="text-gray-700 font-medium mb-1">Email</label>
+          <input
+            type="email"
+            placeholder="example@gmail.com"
+            className="border border-gray-300 rounded-lg px-4 py-2 outline-none 
+                       focus:ring-2 focus:ring-blue-500 focus:border-transparent 
+                       transition duration-300"
+          />
+        </div>
+
+       
+        <div className="flex flex-col">
+          <label className="text-gray-700 font-medium mb-1">Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            className="border border-gray-300 rounded-lg px-4 py-2 outline-none 
+                       focus:ring-2 focus:ring-blue-500 focus:border-transparent 
+                       transition duration-300"
+          />
+        </div>
+
+       
+        <div className="flex justify-between items-center text-sm">
+          <label className="flex items-center gap-2 text-gray-700">
+            <input
+              type="checkbox"
+              className="w-4 h-4 accent-blue-600 focus:ring-2 focus:ring-blue-500"
+            />
+            Remember me
+          </label>
+          <a href="#" className="text-blue-600 hover:underline">
+            Forgot password?
+          </a>
+        </div>
+
+       
+        <button
+          type="button"
+          className="w-full py-2 rounded-lg text-white font-semibold bg-red-700 
+                     hover:bg-blue-800 transition-all duration-300"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
